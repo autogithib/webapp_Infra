bbac30cb0f5cf7c0633376acc9a21ec98470ddcd
autogithib <55689101+autogithib@users.noreply.github.com>
2019-09-24 11:31:39 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,7 +1,7 @@
 provider "azurerm" {
     version = "1.28"
-    subscription_id = "${var.subscription_id}"
-    client_id       = "${var.client_id}"
-    client_secret   = "${var.client_secret}"
-    tenant_id       = "${var.tenant_id}"
+    subscription_id = "${var.azure_subscription_id}"
+    client_id       = "${var.azure_client_id}"
+    client_secret   = "${var.azure_client_secret}"
+    tenant_id       = "${var.azure_tenant_id}"
 }
